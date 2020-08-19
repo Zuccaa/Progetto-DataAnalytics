37d7775475ba440cf929cf5775e1436b928ef7b8
Stefano Zuccarella <s.zuccarella1@campus.unimib.it>
2020-08-19 18:38:38 +0200

    Creato script per produrre il grafo delle fermate

--- CreateTable.py
+++ CreateTable.py
@@ -1,8 +1,9 @@
 import pandas as pd
-import numpy as np
+import json
 
 stop_times = pd.read_csv('trenord-gtfs-csv//stop_times.csv')
 stop_times = stop_times.drop(['stop_headsign', 'pickup_type', 'drop_off_type', 'shape_dist_traveled'], axis=1)
+stop_times = stop_times.sort_values(by=['trip_id', 'stop_sequence']).reset_index(drop=True)
 routes = pd.read_csv('trenord-gtfs-csv//routes.csv')
 
 trips = pd.read_csv('trenord-gtfs-csv//trips.csv')
@@ -16,20 +17,48 @@ for index, row in trips.iterrows():
     else:
         trips_per_route[row['route_id']] = [row['trip_id']]
 
-'''import json
-
+'''
 with open('trips_per_route.json', 'w') as fp:
     json.dump(trips_per_route, fp)'''
 
-route_stops = np.zeros((len(trips_per_route), 42))
+routes_dict = {}
 
-i = 0
+print("Lunghezza iniziale", len(stop_times.index))
 for key in trips_per_route.keys():
-    for trip in trips_per_route[key]:
-        for index, row in stop_times.iterrows():
+    routes_dict[key] = []
+    for index, row in stop_times.iterrows():
+        if row['trip_id'] in trips_per_route[key]:
+            if index != stop_times.index[-1]:
+                next_row = stop_times.loc[index + 1]
+                if next_row['stop_sequence'] == row['stop_sequence'] + 1:
+                    edge = {row['stop_id'], next_row['stop_id']}
+                    if edge not in routes_dict[key]:
+                        routes_dict[key].append(edge)
+            stop_times = stop_times.drop(index)
+    stop_times = stop_times.reset_index(drop=True)
+    print(key, " fatta, lunghezza di stop_times ora è ", len(stop_times.index))
+
+'''with open('routes_dict.txt', 'w') as f:
+    print(routes_dict, file=f)'''
+
+import csv
+
+with open('routes_dict.csv', 'w', newline='') as file:
+    fieldnames = ['Linea', 'Stazione 1', 'Stazione 2']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for key in routes_dict.keys():
+        for set_list in routes_dict[key]:
+            writer.writerow({'Linea': key, 'Stazione 1': set_list.pop(), 'Stazione 2': set_list.pop()})
+
+'''i = 0
+for key in trips_per_route.keys():
+    for index, row in stop_times.iterrows():
+        if row['trip_id'] in trips_per_route[key]:
             element = route_stops[i, row['stop_sequence'] - 1]
-            if row['trip_id'] == trip:
-                if row['stop_id'] != element and element != 0:
-                    print("Ciao ", key, " ", row['stop_id'], " ", element, " ", row['stop_sequence'] - 1)
-                    element = row['stop_id']
-    i = i + 1
+            if row['stop_id'] != element and element != 0:
+                print("Ciao ", key, " ", row['stop_id'], " ", element, " ", row['stop_sequence'] - 1)
+            route_stops[i, row['stop_sequence'] - 1] = row['stop_id']
+            stop_times.drop(index)
+    print("Linea ", key, " fatta!")'''
