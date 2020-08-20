0fd50ae493f71bf2bd4a0449829690dcc57d560e
Stefano Zuccarella <s.zuccarella1@campus.unimib.it>
2020-08-20 18:38:04 +0200

    Completato file per creare il grafo delle stazioni

--- CreateTable.py
+++ CreateTable.py
@@ -1,25 +1,29 @@
 import pandas as pd
-import json
+import numpy as np
+import csv
 
 stop_times = pd.read_csv('trenord-gtfs-csv//stop_times.csv')
-stop_times = stop_times.drop(['stop_headsign', 'pickup_type', 'drop_off_type', 'shape_dist_traveled'], axis=1)
+stop_times = stop_times.drop(['stop_headsign', 'pickup_type',
+                              'drop_off_type', 'shape_dist_traveled'], axis=1)
 stop_times = stop_times.sort_values(by=['trip_id', 'stop_sequence']).reset_index(drop=True)
+
+stop_times_load = stop_times.copy()
+stop_times_load = stop_times_load.drop(['stop_sequence'], axis=1)
+
 routes = pd.read_csv('trenord-gtfs-csv//routes.csv')
+routes = routes.drop(['agency_id', 'route_short_name', 'route_desc', 'route_type', 'route_url',
+                      'route_text_color'], axis=1)
 
 trips = pd.read_csv('trenord-gtfs-csv//trips.csv')
-trips = trips.drop(['trip_headsign', 'direction_id', 'block_id', 'shape_id'], axis=1)
+trips = trips.drop(['trip_headsign', 'trip_short_name', 'direction_id', 'block_id', 'shape_id'], axis=1)
 
-trips_per_route = {}
+'''trips_per_route = {}
 
 for index, row in trips.iterrows():
     if row['route_id'] in trips_per_route:
         trips_per_route[row['route_id']].append(row['trip_id'])
     else:
         trips_per_route[row['route_id']] = [row['trip_id']]
-
-'''
-with open('trips_per_route.json', 'w') as fp:
-    json.dump(trips_per_route, fp)'''
 
 routes_dict = {}
 
@@ -38,27 +42,46 @@ for key in trips_per_route.keys():
     stop_times = stop_times.reset_index(drop=True)
     print(key, " fatta, lunghezza di stop_times ora è ", len(stop_times.index))
 
-'''with open('routes_dict.txt', 'w') as f:
-    print(routes_dict, file=f)'''
-
-import csv
-
 with open('routes_dict.csv', 'w', newline='') as file:
-    fieldnames = ['Linea', 'Stazione 1', 'Stazione 2']
+    fieldnames = ['Linea', 'Stazione 1', 'Stazione 2', 'Colore']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
 
     writer.writeheader()
     for key in routes_dict.keys():
+        selected_row = routes.loc[routes['route_id'] == key]
+        color = '#' + selected_row['route_color'].values[0]
         for set_list in routes_dict[key]:
-            writer.writerow({'Linea': key, 'Stazione 1': set_list.pop(), 'Stazione 2': set_list.pop()})
+            writer.writerow({'Linea': key, 'Stazione 1': set_list.pop(),
+                             'Stazione 2': set_list.pop(), 'Colore': color})'''
 
-'''i = 0
-for key in trips_per_route.keys():
-    for index, row in stop_times.iterrows():
-        if row['trip_id'] in trips_per_route[key]:
-            element = route_stops[i, row['stop_sequence'] - 1]
-            if row['stop_id'] != element and element != 0:
-                print("Ciao ", key, " ", row['stop_id'], " ", element, " ", row['stop_sequence'] - 1)
-            route_stops[i, row['stop_sequence'] - 1] = row['stop_id']
-            stop_times.drop(index)
-    print("Linea ", key, " fatta!")'''
+calendar = pd.read_csv('trenord-gtfs-csv//calendar.csv')
+calendar = calendar.drop(['start_date', 'end_date'], axis=1)
+
+loads = trips.merge(calendar, on='service_id').copy()
+loads = loads.merge(stop_times_load, on='trip_id')
+
+np.savetxt(r'loads.txt', loads.values, fmt='%s')
+
+frequencies_dict = {}
+day = 'monday'
+hour1 = '17:00:00'
+hour2 = '18:00:00'
+
+'''for index, row in loads.iterrows():
+    key = row['stop_id']
+    if row[day] == 1:
+        if key not in frequencies_dict:
+            frequencies_dict[key] = 1
+        else:
+            frequencies_dict[key] += 1'''
+
+for index, row in loads.iterrows():
+    key = row['stop_id']
+    if row[day] == 1 and hour1 < row['arrival_time'] < hour2:
+        if key not in frequencies_dict:
+            frequencies_dict[key] = 1
+        else:
+            frequencies_dict[key] += 1
+
+sorted_frequencies = sorted(frequencies_dict.items(), key=lambda x: x[1], reverse=True)
+print(sorted_frequencies)
